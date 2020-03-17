9d186ba979fbe45ab8f5228c26a802b4e79797d7
JSLee <jasoc1004@gmail.com>
2020-03-17 13:02:18 +0900

    init repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RenderAfterNavermapsLoaded } from 'react-naver-maps';
+import Map from './Map';
 
+const API_KEY = `${process.env.REACT_APP_API_KEY_MAP}`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RenderAfterNavermapsLoaded
+        ncpClientId={API_KEY}
+        error={<p>Maps Load Fail</p>}
+        loading={<p>Maps Loading...</p>}
+      >
+        <Map />
+      </RenderAfterNavermapsLoaded>
     </div>
   );
 }
